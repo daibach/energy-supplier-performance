709b88c2ee734143286ef5b80534fd6fa58ad5e4
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-05-19 18:56:48 +0100

    install codeigniter 2.1.3

---
+++ application/core/DEFAULT_Controller.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+//application/libraries/MY_Controller.php
+
+class DEFAULT_Controller extends CI_Controller {
+
+  function __construct() {
+    parent::__construct();
+    session_start();
+    if (SITE_MAINTENANCE) {
+      include('maintenance.php');
+      exit;
+    }
+
+  }
+
+  function _do_cache() {
+    if(SITE_CACHE) {
+      $this->output->cache(SITE_CACHE_TIME);
+    }
+  }
+
+}
