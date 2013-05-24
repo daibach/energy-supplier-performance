6c3404d2ec2869a92d772b01171a609ae902cd4f
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-05-24 18:17:52 +0100

    Refactor password helper
    Only define the functions if they don't already
    exist

--- application/helpers/password_helper.php
+++ application/helpers/password_helper.php
@@ -4,28 +4,32 @@
  * Generate a new salt
  *
  * @access   public
- */    
-function generate_salt($max_length=10, $any_text="")
-{
+ */
+if ( ! function_exists('generate_salt')) {
+  function generate_salt($max_length=10, $any_text="") {
+    $base_salt = random_string('unique');
+    if($any_text != "") { $base_salt = $any_text; }
 
-  $base_salt = random_string('unique');
-  if($any_text != "") { $base_salt = $any_text; }
+    $salt = hash('md5',$base_salt.date('YmdHis'));
 
-  $salt = hash('md5',$base_salt.date('YmdHis'));
+    if($max_length > 0) {
+      $salt = substr($salt,0,$max_length);
+    }
 
-  if($max_length > 0) {
-    $salt = substr($salt,0,$max_length);
+    return $salt;
   }
-
-  return $salt;
 }
 
-function generate_password($max_legnth) {
-  return random_string('alnum',10);
+if ( ! function_exists('generate_password')) {
+  function generate_password($max_legnth) {
+    return random_string('alnum',10);
+  }
 }
 
-function encrypt_password($password,$salt) {
-  return sha1($password.$salt);
+if ( ! function_exists('encrypt_password')) {
+  function encrypt_password($password,$salt) {
+    return sha1($password.$salt);
+  }
 }
 
 
