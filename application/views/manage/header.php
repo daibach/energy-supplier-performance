70adde2ac92e3655df8c76371843f2aa234448c7
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-05-24 13:07:35 +0100

    Use minified css

--- application/views/manage/header.php
+++ application/views/manage/header.php
@@ -20,8 +20,8 @@
     <?php endif;?>
 
 
-    <link href="/css/bootstrap.css" rel="stylesheet">
-    <link href="/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-responsive.min.css" rel="stylesheet">
     <link href="/css/manage.css" rel="stylesheet">
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
