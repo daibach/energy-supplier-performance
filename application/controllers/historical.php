8427a99241eff29aad2bfbe997b826aa96b5946e
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-07-15 08:48:56 +0100

    Display historical data table on historical page

--- application/controllers/historical.php
+++ application/controllers/historical.php
@@ -19,7 +19,10 @@ class Historical extends ESP_Controller {
       )
     );
 
-    $data = array();
+    $data = array(
+      'suppliers' => $this->suppliers->all(),
+      'historical_period_data' => $this->_generate_historical_data()
+    );
 
     $this->load->view('template/header',$pagedata);
     $this->load->view('historical_list',$data);
@@ -52,6 +55,60 @@ class Historical extends ESP_Controller {
     } else {
       show_404(current_url());
     }
+  }
+
+  function _generate_historical_data() {
+    $suppliers = $this->suppliers->all();
+    $periods = $this->periods->all();
+
+    if( ! empty($periods)) {
+      $historical_periods = array();
+
+      foreach($periods as $period) {
+        $period_data = $this->period_data->all_for_period($period->id,false,'name');
+
+        $month1 = $month2 = $month3 = array(
+          'month' => 0,
+          'month_name' => '',
+          'year' => $period->period_year,
+          'quarter' => $period->period_quarter,
+          'data' => array()
+        );
+        $month1['month'] = identify_quarter_month($period->period_quarter,1,'n');
+        $month1['month_name'] = identify_quarter_month($period->period_quarter,1,'short');
+        $month2['month'] = identify_quarter_month($period->period_quarter,2,'n');
+        $month2['month_name'] = identify_quarter_month($period->period_quarter,2,'short');
+        $month3['month'] = identify_quarter_month($period->period_quarter,3,'n');
+        $month3['month_name'] = identify_quarter_month($period->period_quarter,3,'short');
+
+        foreach($period_data as $period_supplier_data) {
+          $m1 = $m2 = $m3 = array(
+            'supplier' => $period_supplier_data->supplier_name,
+            'supplier_slug' => $period_supplier_data->supplier_slug,
+            'value' => 0,
+            'ranking' => $period_supplier_data->ranking
+          );
+          $m1['value'] = $period_supplier_data->month1;
+          $m2['value'] = $period_supplier_data->month2;
+          $m3['value'] = $period_supplier_data->month3;
+
+          $month1['data'][$period_supplier_data->supplier_id] = $m1;
+          $month2['data'][$period_supplier_data->supplier_id] = $m2;
+          $month3['data'][$period_supplier_data->supplier_id] = $m3;
+
+        }
+
+        array_push($historical_periods,$month1);
+        array_push($historical_periods,$month2);
+        array_push($historical_periods,$month3);
+
+      }
+      return $historical_periods;
+
+    } else {
+      return false;
+    }
+
   }
 }
 
--- application/views/historical_list.php
+++ application/views/historical_list.php
@@ -7,6 +7,31 @@
 
     <div id="graph"></div>
 
+    <table class="table">
+      <thead>
+        <tr>
+          <th class="monthname">Month</th>
+          <?php foreach($suppliers as $supplier) : ?>
+            <th><a href="<?php echo site_url(array('supplier',$supplier->supplier_slug)); ?>"><?php echo $supplier->supplier_short_name; ?></a></th>
+          <?php endforeach; ?>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($historical_period_data as $period) : ?>
+          <tr>
+            <th><a href="<?php echo site_url(array('historical',$period['year'].'-q'.$period['quarter'])); ?>"><?php echo $period['month_name']." ".$period['year']; ?></a></th>
+            <?php foreach($suppliers as $supplier) : ?>
+              <td class="suppliervalue"><?php echo $period['data'][$supplier->id]['value']; ?>
+                <?php if( $supplier->supplier_slug != 'average') : ?>
+                  <br/><span class="badge badge-<?php echo ranking_css_class($period['data'][$supplier->id]['ranking']); ?>"><?php echo add_ordinal_suffix($period['data'][$supplier->id]['ranking']); ?></span>
+                <?php endif; ?>
+              </td>
+            <?php endforeach; ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+
   </div>
   <div class="span3">
     <div class="well">
